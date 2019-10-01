58795760ddefdf8ba14bc894ade49bb4aaad9928
“marses” <marko.seslija@gmail.com>
2019-10-01 13:28:22 +0100

    add requirements for test

---
+++ tests/test_sample.py
@@ -0,0 +1,13 @@
+import numpy
+import pytest
+from tiltx.data_generator import DataGenerator
+from tiltx.feature_extractor import FeatureExtractor
+
+
+@pytest.mark.parametrize("RT_method", ['cumsum'])
+def test_example1(RT_method):
+    """ """
+    t, alpha, beta = DataGenerator.example(1)
+    features = FeatureExtractor(t,alpha,beta,RT_method='cumsum',correct='up')
+    assert numpy.isclose(features.RT, 0.669, rtol=0.05)
+
