ae79c4125bef8fc8c7a65508f5ce65f401ddb0d5
“marses” <marko.seslija@gmail.com>
2019-10-01 13:50:14 +0100

    add requirements for test

--- setup.py
+++ setup.py
@@ -16,10 +16,10 @@ setuptools.setup(
     url="https://github.com/marses/tiltx",
     packages=setuptools.find_packages(),
     install_requires=requirements,
-    tests_requires=["pytest"]
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
     ],
+    tests_requires=["pytest"],
     python_requires='>=3.6',
 )
